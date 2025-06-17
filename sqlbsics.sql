123de0d8e56b5429d4eb7beee60837a8e11ef3da
Aakash <editsky597@gmail.com>
2025-06-17 21:23:07 +0530

    sql basics

---
+++ sqlbsics.sql
@@ -0,0 +1,50 @@
+create database temp1;
+
+CREATE DATABASE tem2;
+CREATE DATABASE tem2;
+CREATE DATABASE if not exists tem2;
+
+drop database  tem2;
+drop database if exists tem2;
+
+
+drop database tem2; 
+drop database temp1;
+
+
+create database college;
+
+use college;
+
+create table student(
+id int primary key,
+name varchar(50),
+age int not null
+);
+
+insert into student values(1,"Sky",20);
+insert into student values(2,"Aakash",21);
+insert into student values(3,"Aaku",21);
+
+
+
+show databases;
+show tables;
+select * from student;
+
+
+create database d3;
+
+use d3;
+
+create table delulu(
+id int primary key,
+name varchar(50));
+
+
+insert into delulu values(1,"desginer");
+insert into delulu values(2,"developer");
+
+select * from delulu;
+
+
