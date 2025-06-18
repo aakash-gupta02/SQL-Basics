4746d2fb72d938dbb7b67af31933636fc7715414
Aakash <editsky597@gmail.com>
2025-06-19 00:01:40 +0530

    exclusive joins * self join

--- join.sql
+++ join.sql
@@ -59,13 +59,67 @@ union
 select * 
 from students1 
 right join courses
-on students1.course_id = courses.course_id;
+on students1.course_id = courses.course_id; 
 
 
 
 
+# exlucsive right or left join 
 
 
+
+select * 
+from students1 as a 
+left join courses as b
+on a.course_id = b.course_id
+where b.course_id is null;
+
+
+
+
+select * 
+from students1 as a 
+right join courses as b
+on a.course_id = b.course_id
+where a.course_id is null
+union
+select * 
+from students1 as a 
+left join courses as b
+on a.course_id = b.course_id
+where b.course_id is null;
+
+
+
+# self join 
+
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  name VARCHAR(100),
+  manager_id INT
+);
+
+
+
+
+INSERT INTO employees (id, name, manager_id) VALUES
+(1, 'Amit', NULL),         #  Top-level manager (CEO)
+(2, 'Riya', 1),
+(3, 'Karan', 1),
+(4, 'Sneha', 2),
+(5, 'Ravi', 2),
+(6, 'Anjali', 3),
+(7, 'Pooja', 3),
+(8, 'Nikhil', 4);
+
+
+
+select * from employees;
+
+select a.name as manager_name, b.name 
+from employees as a
+join employees as b
+on a.id = b.manager_id;
 
 
 
